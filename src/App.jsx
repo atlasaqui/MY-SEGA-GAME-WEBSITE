5236a0516f7b806d82860950c0d645c4eaa5a087
JetSetProject <victorwmonteiro@gmail.com>
2026-03-20 13:41:29 -0300

    Configurei a paleta de cores no '@theme'

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = () => {
+    return (
+        <main>
+            <h1 className="text-5xl text-orange-500 font-bold font-jetset">Welcome to Jet Set Radio</h1>
+        </main>
+    )
+}
+export default App
